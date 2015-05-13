a463fe25f21194744e9979840b7535f6cd765e36
Christoph Diehl <cdiehl@mozilla.com>
2015-05-13 18:04:36 +0200

    Use shlex to split parameters

--- core/plugins/command.py
+++ core/plugins/command.py
@@ -3,6 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 import os
+import shlex
 
 from ..quokka import ExternalProcess, PluginException
 
@@ -24,6 +25,6 @@ class ConsoleApplication(ExternalProcess):
 
         cmd = [binary]
         if params:
-            cmd.append(params)
+            cmd.extend(shlex.split(params))
 
         self.process = self.open(cmd, environ)
